abc670c9a0d279c267d64fa0b8e28ae5dde4f8be
Melisa Calderon <mecalderon@vassar.edu>
2021-06-30 19:34:08 +0000

    converted to functions

--- bad_flow.py
+++ bad_flow.py
@@ -1,9 +1,8 @@
-from sqlalchemy import create_engine
 import requests
 import spotipy
 import pandas as pd
-import sqlalchemy 
-
+import sqlalchemy
+from sqlalchemy import create_engine
 
 CLIENT_ID = '7bb5a610971f437690b91981206e0025'
 CLIENT_SECRET = 'f102316e6e7b45bd93e1988be3162cf6'
@@ -22,36 +21,74 @@ access_token = auth_response_data['access_token']
 headers = {
   'Authorization': 'Bearer {token}'.format(token=access_token)
 }
+
 artist_id = '5cj0lLjcoR7YOSnhnX0Po5'
 BASE_URL = 'https://api.spotify.com/v1/'
-r = requests.get(BASE_URL + 'artists/' + artist_id + '/albums',
-                 headers=headers, params={'limit': 10})
-response = r.json()
 title = "Doja Cat's last Ten Released Albums"
-print(title)
-print('-' * len(title))
-for info in response['items']:
-    print(info['name'] + ' was released on ' + info['release_date']
-          + " with " + str(info['total_tracks']) + ' total tracks.')
 
-track = requests.get(BASE_URL + 'artists/' + artist_id
-                     + '/top-tracks?market=us', headers=headers)
-tracks = track.json()
+#Test  1: check if artist_id is not vaild
+#Test  2: check if base url is vaild
+#Test  2: check if json worked
+def convertToJson(BASE_URL, artist_id):
+        r = requests.get(BASE_URL + 'artists/' + artist_id + '/albums',
+                       headers=headers, params={'limit': 10})
+        return r.json()
+
+response = convertToJson(BASE_URL, artist_id)
+
+#Test  1: Check if display looks like its supposed to
+def displayTitle(title):
+        print(title)
+        print('-' * len(title))
+
+#Test  1: check if the title is displayed correctly
+#Test  2: check if response is in correct format
+#Test  2: check if print is printed correctly
+def displayAlbum(title, response):
+        displayTitle(title)
+        for info in response['items']:
+            print(info['name'] + ' was released on ' + info['release_date']
+                  + " with " + str(info['total_tracks']) + ' total tracks.')
+
+
+displayAlbum(title, response)
+
+#Test  1: check if artist_id is not vaild
+#Test  2: check if base url is vaild
+#Test  2: check if json worked
+def convertToJsonTrack(BASE_URL, artist_id):
+        track = requests.get(BASE_URL + 'artists/' + artist_id
+                            + '/top-tracks?market=us', headers=headers)
+        return track.json()
+
+
+tracks = convertToJsonTrack(BASE_URL, artist_id)
 ntitle = 'Top Tracks in the US'
-print(ntitle)
-print('-' * len(ntitle))
-top_tracks = {}
-i =0
-for t in tracks['tracks']:
-    top_tracks[i] = [t['name'], t['explicit'], t['popularity']]
-    i += 1
-    if t['explicit']:
-        print(t['name'] + ' which has a popularity of ' + str(t['popularity'])
-              + ' and is explicit.')
-    else:
-        print(t['name'] + ' which has a popularity of ' + str(t['popularity']))
 
-df = pd.DataFrame.from_dict(top_tracks, orient = 'index', columns=['Track_Name', 'Explicit', 'Popularity'])
+#Test  1: check if the title is displayed correctly
+#Test  2: check if response is in correct format
+#Test  2: check if print is printed correctly
+def displayTopTracks(title, tracks):
+        displayTitle(title)
+        top_tracks = {}
+        i = 0
+        for t in tracks['tracks']:
+            top_tracks[i]= [t['name'], t['explicit'], t['popularity']]
+            i += 1
+            if t['explicit']:
+                print(t['name'] + ' which has a popularity of ' + str(t['popularity'])
+                      + ' and is explicit.')
+            else:
+                print(t['name'] + ' which has a popularity of ' + str(t['popularity']))
+        return top_tracks
 
-engine = create_engine('mysql://root:codio@localhost/spotifydoja')
-df.to_sql('Top_Tracks', con=engine, if_exists='replace', index=False)
+top_tracks = displayTopTracks(ntitle, tracks)
+
+def convertToDataFrame(top_tracks):
+        df = pd.DataFrame.from_dict(top_tracks, orient = 'index',
+                                    columns=['Track_Name', 'Explicit', 'Popularity'])
+        engine = create_engine('mysql://root:codio@localhost/spotifydoja')
+        df.to_sql('Top_Tracks', con=engine, if_exists='replace', index=False)
+
+convertToDataFrame(top_tracks)
+
